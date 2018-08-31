1f2ba910058343ba37ad6e8d2d59feebed137379
Taner Candarli <tcandarli@gmail.com>
2018-08-31 17:08:46 +0200

    Add files via upload

---
+++ JS_training_ES_updates2/JS_ES6_2.js
@@ -0,0 +1,59 @@
+// Arrays
+
+var x = Array.from("JavaScript");
+console.log(x);
+
+// ES5
+
+// var ages = [12, 17, 8, 21, 14, 11];
+// var full = ages.map(function(cur){
+//     return cur >= 18;
+// });
+
+// console.log(full);
+// console.log(full.indexOf(true));
+// console.log(ages[full.indexOf(true)]);
+
+// ES6
+// findindex() = returns the index of the first element in the array that satisfies the provided texting function
+// find() = returns the value of the first element in the array that satisfies the provided testing function
+
+// let ages = [12, 17, 8, 21, 14, 11];
+// console.log(ages.findIndex(a => a >= 18));
+// console.log(ages.find(a => a >= 18));
+
+
+
+// for..of loop
+
+// let myArray = [10, 20, 30];
+// for (let x of myArray) {
+//     x *= 2;
+//     console.log(x);
+// };
+
+
+// let myObj = {
+//     x : 1,
+//     y: 2,
+//     z: 3
+// }
+
+// for (let a in myObj) {
+//     console.log(a);
+//     console.log(myObj[a]);
+
+// }
+
+// let list = [4, 5, 6];
+// for (let i in list) {
+//     console.log(list[i]);
+// }
+
+let list = [4, 5, 6];
+for (let i of list) {
+    console.log(i);
+}
+
+// Spread Operator
+
